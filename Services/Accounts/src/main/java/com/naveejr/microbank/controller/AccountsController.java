302acf6d712fffe28cada8bc2d27438f8c52b1ca
rajeevan <naveejr@gmail.com>
2023-03-09 00:45:56 +0530

    Accounts Service uses config server.

--- Services/Accounts/src/main/java/com/naveejr/microbank/controller/AccountsController.java
+++ Services/Accounts/src/main/java/com/naveejr/microbank/controller/AccountsController.java
@@ -1,13 +1,16 @@
 package com.naveejr.microbank.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.naveejr.microbank.bo.Account;
+import com.naveejr.microbank.config.AccountServiceConfig;
 import com.naveejr.microbank.dto.CustomerDTO;
+import com.naveejr.microbank.dto.Properties;
 import com.naveejr.microbank.repository.AccountsRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RequiredArgsConstructor
 
@@ -17,9 +20,19 @@ public class AccountsController {
 
 	private final AccountsRepository accountsRepository;
 
+	private final AccountServiceConfig accountServiceConfig;
+
 	@PostMapping("myAccount")
 	public Account getAccountDetails(@RequestBody CustomerDTO customerDTO) {
 		return accountsRepository.findByCustomerId(customerDTO.id());
+	}
+
+	@GetMapping("properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountServiceConfig.getMsg(), accountServiceConfig.getBuildVersion(),
+				accountServiceConfig.getMailDetails(), accountServiceConfig.getActiveBranches());
+		return ow.writeValueAsString(properties);
 	}
 
 }
