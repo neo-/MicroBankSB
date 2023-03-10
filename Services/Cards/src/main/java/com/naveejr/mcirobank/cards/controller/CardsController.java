e4da1b90147f113c055b0b709983d396f10410b9
rajeevan <naveejr@gmail.com>
2023-03-10 14:52:20 +0530

    Cards and Loans services using config server

--- Services/Cards/src/main/java/com/naveejr/mcirobank/cards/controller/CardsController.java
+++ Services/Cards/src/main/java/com/naveejr/mcirobank/cards/controller/CardsController.java
@@ -1,13 +1,15 @@
 package com.naveejr.mcirobank.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.naveejr.mcirobank.cards.bo.Card;
+import com.naveejr.mcirobank.cards.config.CardsServiceConfig;
 import com.naveejr.mcirobank.cards.dto.CustomerDTO;
+import com.naveejr.mcirobank.cards.dto.Properties;
 import com.naveejr.mcirobank.cards.repository.CardsRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +21,20 @@ public class CardsController {
 
 	private final CardsRepository cardsRepository;
 
+	private final CardsServiceConfig cardsServiceConfig;
+
 	@PostMapping("myCards")
 	public List<Card> getCardsDetails(@RequestBody CustomerDTO customerDTO) {
 		return cardsRepository.findByCustomerId(customerDTO.id());
+	}
+
+
+	@GetMapping("properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(),
+				cardsServiceConfig.getMailDetails(), cardsServiceConfig.getActiveBranches());
+		return ow.writeValueAsString(properties);
 	}
 
 }
--- Services/Loans/src/main/java/com/naveejr/microbank/loans/controller/LoanController.java
+++ Services/Loans/src/main/java/com/naveejr/microbank/loans/controller/LoanController.java
@@ -1,13 +1,15 @@
 package com.naveejr.microbank.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.naveejr.microbank.loans.bo.Loan;
+import com.naveejr.microbank.loans.config.LoansServiceConfig;
 import com.naveejr.microbank.loans.dto.CustomerDTO;
+import com.naveejr.microbank.loans.dto.Properties;
 import com.naveejr.microbank.loans.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +20,19 @@ import java.util.List;
 public class LoanController {
 	private final LoanRepository loanRepository;
 
+	private final LoansServiceConfig loansServiceConfig;
+
 	@PostMapping("myLoans")
 	public List<Loan> getLoanDetails(@RequestBody CustomerDTO customer) {
 		return loanRepository.findByCustomerId(customer.id());
 	}
+
+	@GetMapping("properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansServiceConfig.getMsg(), loansServiceConfig.getBuildVersion(),
+				loansServiceConfig.getMailDetails(), loansServiceConfig.getActiveBranches());
+		return ow.writeValueAsString(properties);
+	}
+
 }
