50386030b69f02863f41da906f4b011dca435b3a
rajeevan <naveejr@gmail.com>
2023-03-17 22:38:03 +0530

    Rewrite URLs

---
+++ Services/GatewayServer/src/main/java/com/naveejr/config/RouteLocatorConfiguration.java
@@ -0,0 +1,34 @@
+package com.naveejr.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+@Configuration
+public class RouteLocatorConfiguration {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/microbank/accounts/**")
+						.filters(f -> f.rewritePath("/microbank/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/microbank/loans/**")
+						.filters(f -> f.rewritePath("/microbank/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/microbank/cards/**")
+						.filters(f -> f.rewritePath("/microbank/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://CARDS"))
+				.build();
+	}
+
+}
