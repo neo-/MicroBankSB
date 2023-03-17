335c3a27fe6aa2bf9adfd3ceacf97e748cd6088a
rajeevan <naveejr@gmail.com>
2023-03-17 22:57:54 +0530

    Refactor

--- Services/GatewayServer/src/main/java/com/naveejr/gatewayserver/config/RouteLocatorConfiguration.java
+++ Services/GatewayServer/src/main/java/com/naveejr/gatewayserver/config/RouteLocatorConfiguration.java
@@ -1,4 +1,4 @@
-package com.naveejr.config;
+package com.naveejr.gatewayserver.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
