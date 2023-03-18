37a2b381f96190d484f8df37b2b5a9ede21d9e96
rajeevan <naveejr@gmail.com>
2023-03-18 17:01:56 +0530

    Docker configuration to include gateway server

--- Scripts/dockerBuildAndPush.sh
+++ Scripts/dockerBuildAndPush.sh
@@ -1,13 +1,15 @@
-docker rmi naveejr/microbank-cards:latest
-docker rmi naveejr/microbank-loans:latest
 docker rmi naveejr/microbank-accounts:latest
+docker rmi naveejr/microbank-cards:latest
 docker rmi naveejr/microbank-configserver:latest
 docker rmi naveejr/microbank-eurekaserver:latest
+docker rmi naveejr/microbank-gatewayserver:latest
+docker rmi naveejr/microbank-loans:latest
 
-mvn spring-boot:build-image
+mvn spring-boot:build-image -DskipTests
 
-docker push naveejr/microbank-cards:latest
-docker push naveejr/microbank-loans:latest
 docker push naveejr/microbank-accounts:latest
+docker push naveejr/microbank-cards:latest
 docker push naveejr/microbank-configserver:latest
 docker push naveejr/microbank-eurekaserver:latest
+docker push naveejr/microbank-gatewayserver:latest
+docker push naveejr/microbank-loans:latest
